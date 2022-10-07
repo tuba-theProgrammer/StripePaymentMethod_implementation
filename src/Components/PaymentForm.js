ea10a7e2fcfb1adad5ef604910c412cc26f49a7f
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-10-07 14:36:39 +0500

    pacakages update

--- src/Components/PaymentForm.js
+++ src/Components/PaymentForm.js
@@ -30,12 +30,23 @@ export default function PaymentForm(){
    const [success,setSuccess] = React.useState(false)
    const stripe = useStripe()
    const elements = useElements()
+   const [email, setEmail] = React.useState('');
 
    const handleSubmit = async (e)=>{
+
     e.preventDefault()
+
+    if(!stripe||!elements){
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
     const {error,paymentMethod} = await stripe.createPaymentMethod({
         type:"card",
-        card: elements.getElement(CardElement)
+        card: elements.getElement(CardElement),
+        billing_details:{
+                email:email
+        }
     })
 
      if(!error){
@@ -43,12 +54,18 @@ export default function PaymentForm(){
            const {id} = paymentMethod
            const response = await axios.post("http://localhost:3001/payment",{
             amount:1000,
-            id
+            id,
+            email
            })
          
            if(response.data.success){
             console.log("successful payment")
             setSuccess(true)
+             // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
            }
 
         }catch(error){
